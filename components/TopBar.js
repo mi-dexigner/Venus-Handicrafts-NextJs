eb99a5883bfd2a5f7317f7eee0a3406fb8a30999
Muhammad idrees <muhammad.idrees@ossols.local>
2021-12-02 22:28:55 +0500

    add topbar

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,33 +1,15 @@
+import '../styles/TopBar.module.css'
 const TopBar = () => {
     return (
         <section className="topbar d-none d-lg-block d-xl-block">
-      <div className="container">
-        <div className="row align-items-center">
-          <div className="col-md-8">
-            <p>
-              Welcome to our store call
-              <a href="tel//+1123456789">+1 123 4567 89</a> for Support
-            </p>
-          </div>
-          <div className="col-md-4 text-end">
-            <ul className="list-inline">
-              <li className="list-inline-item">
-                <select className="form-select">
-                  <option value="english">English</option>
-                  <option value="spanish">Spanish</option>
-                  <option value="german">German</option>
-                </select>
-              </li>
-              <li className="list-inline-item">
-                <select className="form-select">
-                  <option value="USD">USD</option>
-                  <option value="POUND">POUND</option>
-                  <option value="EURO">EURO</option>
-                </select>
-              </li>
-              <li className="list-inline-item"><a href="#">Login</a></li>
-            </ul>
-          </div>
+      <div className="container-fluid">
+        <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3 align-items-center">
+            <div className="col">1</div>
+            <div className="col">2</div>
+            <div className="col">3</div>
+            <div className="col">4</div>
+            <div className="col">5</div>
+         
         </div>
       </div>
     </section>
--- pages/_document.js
+++ pages/_document.js
@@ -9,16 +9,7 @@ class MyDocument extends Document {
   render() {
     return (
         <Html lang="en">
-        <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin='true' />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600;800&display=swap" rel="stylesheet" crossOrigin='true'/>
-    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
-      rel="stylesheet" />
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" crossOrigin='true' />
-    <link href="https://cdn.jsdelivr.net/gh/mi-dexigner/Venus-Handicrafts-eProject@main/assets/css/all.min.css" rel="stylesheet" crossOrigin='true' />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/mi-dexigner/Venus-Handicrafts-eProject@main/assets/css/styles.css" crossOrigin='true' />
-          </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
